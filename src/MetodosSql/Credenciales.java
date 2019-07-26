fb7f73a35a22cb3c7e6e0d418b81ff5efab622d7
LEOBAINI <leobaini@hotmail.com>
2019-07-26 00:19:47 -0300

    Emprolijando un poco

--- src/MetodosSql/Credenciales.java
+++ src/MetodosSql/Credenciales.java
@@ -4,10 +4,10 @@ public class Credenciales {
 
 	
 	
-	public static String base_CCCC="COMPLETAR";
-	public static String ip_CCCC="127.0.0.1"; //172.31.54.95:v103	10.54.119.139:v 50
-	public static String usuario_CCCC="USUARIO";
-	public static String password_CCCC="PASS";
+	public static String base="COMPLETAR";
+	public static String ip="127.0.0.1"; //172.31.54.95:v103	10.54.119.139:v 50
+	public static String usuario="USUARIO";
+	public static String password="PASS";
 	
 	
 	
